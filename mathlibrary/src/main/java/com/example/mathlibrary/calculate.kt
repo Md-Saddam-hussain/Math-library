dec64f6f18afe53170b9967cb93bc522b6aa5b8b
mdshussa <saddam49291@gmail.com>
2022-08-25 09:43:57 +0530

    math library

---
+++ mathlibrary/src/main/java/com/example/mathlibrary/calculate.kt
@@ -0,0 +1,19 @@
+package com.example.mathlibrary
+
+class calculate {
+    companion object {
+
+        fun addition(a: Int, b: Int): Int {
+            return a + b
+        }
+        fun subtractiontion(a: Int, b: Int): Int {
+            return a - b
+        }
+       fun multiplication(a: Int, b: Int): Int {
+        return a * b
+        }
+        fun division(a: Int, b: Int): Int {
+            return a / b
+        }
+    }
+}
